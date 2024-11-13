1dd88a71e6cdda2b5315af4fdd8c147e0eadee53
javadbat <javadbat@gmail.com>
2024-11-13 18:46:15 +0330

    update path

---
+++ node_modules/bank-indicator/lib/types.ts
@@ -0,0 +1,17 @@
+import {JBPaymentInputWebComponent} from '../../lib/jb-payment-input';
+export type BankIndicatorElements = {
+    bankImageWrapper: HTMLDivElement;
+    PaymentInputParent:JBPaymentInputWebComponent | null;
+}
+type HEX = `#${string}`;
+export type BankItem = {
+    title:{
+        fa:string,
+        en:string
+    },
+    prefix:string[],
+    logo:string,
+    color:HEX,
+    primary:HEX,
+}
+export type BankInfo = BankItem[];
