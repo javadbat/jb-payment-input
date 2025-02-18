0db7ee1016d85ebe5045fbb1d71a141fe0c6abb3
javadbat <javadbat@gmail.com>
2025-02-18 18:41:21 +0330

    update event

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,6 @@
-import { PaymentInputTypeList } from "./constants";
+import type { PaymentInputTypeList } from "./constants";
+import type {JBPaymentInputWebComponent} from './jb-payment-input.js';
+import type {EventTypeWithTarget} from 'jb-core';
 
 export type PaymentInputType = typeof PaymentInputTypeList[number];
+export type JBPaymentInputEventType<TEvent> = EventTypeWithTarget<TEvent,JBPaymentInputWebComponent>;
